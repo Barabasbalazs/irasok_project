5d77e895ad0d54ee5ae8a6e0f57d322d59571c0e
Balazs <barabas.balazska@gmail.com>
2026-05-31 18:57:21 +0200

    desc md is excempt from computed sidebar as well

--- .vitepress/configs/sidebar.ts
+++ .vitepress/configs/sidebar.ts
@@ -1,20 +1,29 @@
 import { type DefaultTheme } from 'vitepress';
 import fs from 'node:fs/promises';
 
+function sanitizeTitle(title: string): string {
+    return title.replace(/(\.md)$/, '')
+}
+
+const excemptWritingsFromSidebar: Record<string, boolean> = {
+    index: true,
+    leírás: true,
+};
+
 let writingsItems: DefaultTheme.SidebarItem[] = [];
 
 try {
     const writingsList = await fs.readdir(`${process.cwd()}/writings`);
     if (writingsList && writingsList.length) {
-        writingsItems = writingsList.filter((item) => !item.includes('index')).map((item) => {
-            const sanitizedTitle = item.replace(/(\.md)$/, '');
+        writingsItems = writingsList.filter((item) => !excemptWritingsFromSidebar[sanitizeTitle(item)]).map((item) => {
+            const sanitizedTitle = sanitizeTitle(item);
             return {
                 text: sanitizedTitle, link: `/${sanitizedTitle}`
             }
         });
     }
 } catch (err) {
-    //error handling?
+    console.warn(err);
 }
 
 const sidebar: DefaultTheme.SidebarItem[] = [
